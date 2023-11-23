f2e573c14d6a60771c0ac9c67745802e02e166a0
Kapustkin Nikita Yurievich <142444812+nikitakapustkin@users.noreply.github.com>
2023-11-23 12:33:05 +0300

    Update circle_tests.py

--- circle_tests.py
+++ circle_tests.py
@@ -20,11 +20,11 @@ class CircleTestCase(unittest.TestCase):
 
     def test_float_area(self):
         res = circle.area(1.2)
-        self.assertAlmostEqual(res, 4.523, delta=0.1)
+        self.assertAlmostEqual(res, 4.523, delta=0.001)
 
     def test_int_area(self):
         res = circle.area(4543543)
-        self.assertAlmostEqual(res, 64854356992636.336, delta=0.1)
+        self.assertAlmostEqual(res, 64854356992636.336, delta=0.001)
 
     def test_str_perimeter(self):
         with self.assertRaises(TypeError):
@@ -36,8 +36,8 @@ class CircleTestCase(unittest.TestCase):
 
     def test_float_perimeter(self):
         res = circle.perimeter(3.4)
-        self.assertAlmostEqual(res, 21.362830044410593, delta=0.1)
+        self.assertAlmostEqual(res, 21.362830044410593, delta=0.001)
 
     def test_int_perimeter(self):
         res = circle.perimeter(325235)
-        self.assertAlmostEqual(res, 2043511.7733805527, delta=0.1)
+        self.assertAlmostEqual(res, 2043511.7733805527, delta=0.001)
