ce79b164fcb59b45c363cc0d0118d978b50553a9
Nikita Kapustkin <nikita.kapustkin2005@gmail.com>
2023-11-23 03:17:20 +0300

    create triangle_tests.py && add tests for triangle in triangle_tests.py

---
+++ triangle_tests.py
@@ -0,0 +1,36 @@
+import unittest
+import triangle
+
+class TriangleTestCase(unittest.TestCase):
+    def test_str_area(self):
+        with self.assertRaises(TypeError):
+            res = triangle.area('2',2)
+
+    def test_float_area(self):
+        res = triangle.area(1.2,1.3)
+        self.assertAlmostEqual(res,0.78,delta = 0.1)
+
+    def test_zero_area(self):
+        res = triangle.area(2,0)
+        self.assertEqual(res,0)
+
+    def test_int_area(self):
+        res = triangle.area(4354353,768768)
+        self.assertEqual(res,1673743623552)
+
+    def test_str_perimeter(self):
+        with self.assertRaises(TypeError):
+            res = triangle.perimeter("1","12","14")
+
+    def test_float_perimeter(self):
+        res = triangle.perimeter(1.2,1.1,1.3)
+        self.assertAlmostEqual(res,3.6,delta = 0.1)
+
+    def test_zero_perim(self):
+        res = triangle.perimeter(0,0,0)
+        self.assertEqual(res,0)
+
+    def test_int_perimeter(self):
+        res = triangle.perimeter(453543,758346,134542)
+        self.assertEqual(res,1346431)
+
